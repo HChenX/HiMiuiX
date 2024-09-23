eff716d0ea52a16544fae44712b8e16cc877bdff
焕晨HChen <HChen_LXi@outlook.com>
2024-09-23 19:33:08 +0800

    添加版权声明

--- src/main/java/com/hchen/himiuix/MiuiSwitchPreference.java
+++ src/main/java/com/hchen/himiuix/MiuiSwitchPreference.java
@@ -1,3 +1,18 @@
+/*
+ * This file is part of HiMiuiX.
+
+ * HiMiuiX is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * HiMiuiX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
+ * Copyright (C) 2023-2024 HiMiuiX Contributions
+ */
 package com.hchen.himiuix;
 
 import static com.hchen.himiuix.MiuiXUtils.sp2px;
